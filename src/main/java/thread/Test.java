6adcf49aadd4fd76b2cbdeee8729e053120b0209
mengbingye <bingyeMeng@gmail.com>
2019-04-24 21:25:20 +0800

    feat: 添加继承Thread方式实现多线程

--- src/main/java/thread/Test.java
+++ src/main/java/thread/Test.java
@@ -7,9 +7,16 @@ public class Test {
 
     public static void main(String[] args) {
 
-        RunnableDemo R1 = new RunnableDemo("Thread-1");
-        R1.start();
-        RunnableDemo R2 = new RunnableDemo("Thread-2");
-        R2.start();
+//        RunnableDemo R1 = new RunnableDemo("Thread-1");
+//        R1.start();
+//        RunnableDemo R2 = new RunnableDemo("Thread-2");
+//        R2.start();
+
+
+        ThreadDemo t1 = new ThreadDemo("Thread-1");
+        t1.start();
+        ThreadDemo t2 = new ThreadDemo("Thread-2");
+        t2.start();
+
     }
 }

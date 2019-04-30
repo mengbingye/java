67ebdc0776a7b3a6659c3ecb13d51c747b7c0865
mengbingye <bingyeMeng@gmail.com>
2019-04-30 21:34:18 +0800

    feat: SynchronousQueue 同步队列

---
+++ src/main/java/thread/blockingqueue/SynchronousQueueExample.java
@@ -0,0 +1,81 @@
+package thread.blockingqueue;
+
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+
+/**
+ * SynchronousQueue 同步队列， 实际上不是一个真正的队列,因为它不会为队列中的元素维护存储空间,他维护的一组线程
+ * ,这些线程在等待着把元素加入或移除队列
+ *
+ * 当有足够多的消费者,并且总有一个消费者准备好获取交付的工作的时，才适合用同步队列
+ */
+public class SynchronousQueueExample {
+
+    static class SynchronousQueueProducer implements Runnable {
+
+        protected BlockingQueue<String> blockingQueue;
+        final Random random = new Random();
+
+        public SynchronousQueueProducer(BlockingQueue<String> queue) {
+            this.blockingQueue = queue;
+        }
+
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    String data = UUID.randomUUID().toString();
+                    System.out.println("Put: " + data);
+                    blockingQueue.put(data);
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    static class SynchronousQueueConsumer implements Runnable {
+
+        protected BlockingQueue<String> blockingQueue;
+
+        public SynchronousQueueConsumer(BlockingQueue<String> queue) {
+            this.blockingQueue = queue;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    String data = blockingQueue.take();
+                    System.out.println(Thread.currentThread().getName()
+                            + " take(): " + data);
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        final BlockingQueue<String> synchronousQueue = new SynchronousQueue<String>();
+
+        SynchronousQueueProducer queueProducer = new SynchronousQueueProducer(
+                synchronousQueue);
+        new Thread(queueProducer).start();
+
+        SynchronousQueueConsumer queueConsumer1 = new SynchronousQueueConsumer(
+                synchronousQueue);
+        new Thread(queueConsumer1).start();
+
+        SynchronousQueueConsumer queueConsumer2 = new SynchronousQueueConsumer(
+                synchronousQueue);
+        new Thread(queueConsumer2).start();
+    }
+}
